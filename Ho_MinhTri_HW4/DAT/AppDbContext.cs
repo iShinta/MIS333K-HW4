893bfafd88be811c58c1b7d6a71864efb549c5d7
iShinta <ho.minhtri.net@gmail.com>
2016-09-27 21:01:56 -0500

    First Commit

---
+++ Ho_MinhTri_HW4/DAT/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Ho_MinhTri_HW4.Models;
+using System.Data.Entity;
+
+namespace Ho_MinhTri_HW3.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        //Constructor that invokes the base constructor
+        public AppDbContext() : base("MyDBConnection") { }
+
+        //Create the db set
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Ho_MinhTri_HW4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ho_MinhTri_HW4.MvcApplication" Language="C#" %>
